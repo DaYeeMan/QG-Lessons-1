523143cf660508161ef98999eede63fefd157e24
DaYeeMan <ensong.zhang@gmail.com>
2025-07-22 21:56:53 -0400

    s

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='static/templates')
+app = Flask(__name__, template_folder='static/templates/level2')
 
 @app.route('/lesson')
 def lesson():
-    return render_template('Limits_and_Continuity.html')
+    return render_template('Moment_Generating_Functions.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 
