15b353dc07b577b103115a9b0a393f9b7c2dd4c1
DaYeeMan <ensong.zhang@gmail.com>
2025-07-29 17:16:52 -0400

    s

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='static/templates/level2')
+app = Flask(__name__, template_folder='static/templates/level3')
 
 @app.route('/lesson')
 def lesson():
-    return render_template('Moment_Generating_Functions.html')
+    return render_template('Stationary_Distributions.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 
