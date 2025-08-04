f7f9b0af6fc41904f5aca0ca38b550b4101f38b1
DaYeeMan2 <ensong.zhang@gmail.com>
2025-08-04 13:48:03 -0400

    s

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='static/templates/level3')
+app = Flask(__name__, template_folder='static/templates/level4')
 
 @app.route('/lesson')
 def lesson():
-    return render_template('Stationary_Distributions.html')
+    return render_template('Continuous_Time_Markov_Chains.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 
